2f11ba4bfdc58938e48655b66a9e34d4de88cb8d
jblankendaal <info@abierto.nl>
2026-06-02 18:06:48 +0200

    feat: add three-phase support to grid meter component

--- components/grid_meter/grid_meter.h
+++ components/grid_meter/grid_meter.h
@@ -27,18 +27,45 @@ struct Client {
 
 class GridMeterComponent : public Component {
  public:
-  GridMeterComponent(sensor::Sensor *power_import, sensor::Sensor *power_export,
-                     sensor::Sensor *voltage, sensor::Sensor *current,
-                     sensor::Sensor *energy_import_t1, sensor::Sensor *energy_import_t2,
-                     sensor::Sensor *energy_export_t1, sensor::Sensor *energy_export_t2)
-      : power_import_(power_import),
-        power_export_(power_export),
-        voltage_(voltage),
-        current_(current),
-        energy_import_t1_(energy_import_t1),
-        energy_import_t2_(energy_import_t2),
-        energy_export_t1_(energy_export_t1),
-        energy_export_t2_(energy_export_t2) {}
+   // Single-phase constructor (for backwards compatibility)
+   GridMeterComponent(sensor::Sensor *power_import, sensor::Sensor *power_export,
+                      sensor::Sensor *voltage, sensor::Sensor *current,
+                      sensor::Sensor *energy_import_t1, sensor::Sensor *energy_import_t2,
+                      sensor::Sensor *energy_export_t1, sensor::Sensor *energy_export_t2)
+       : power_import_(power_import),
+         power_export_(power_export),
+         voltage_(voltage),
+         current_(current),
+         energy_import_t1_(energy_import_t1),
+         energy_import_t2_(energy_import_t2),
+         energy_export_t1_(energy_export_t1),
+         energy_export_t2_(energy_export_t2),
+         is_three_phase_(false) {}
+
+   // Three-phase constructor
+   GridMeterComponent(sensor::Sensor *power_import_l1, sensor::Sensor *power_import_l2, sensor::Sensor *power_import_l3,
+                      sensor::Sensor *power_export_l1, sensor::Sensor *power_export_l2, sensor::Sensor *power_export_l3,
+                      sensor::Sensor *voltage_l1, sensor::Sensor *voltage_l2, sensor::Sensor *voltage_l3,
+                      sensor::Sensor *current_l1, sensor::Sensor *current_l2, sensor::Sensor *current_l3,
+                      sensor::Sensor *energy_import_t1, sensor::Sensor *energy_import_t2,
+                      sensor::Sensor *energy_export_t1, sensor::Sensor *energy_export_t2)
+       : power_import_l1_(power_import_l1),
+         power_import_l2_(power_import_l2),
+         power_import_l3_(power_import_l3),
+         power_export_l1_(power_export_l1),
+         power_export_l2_(power_export_l2),
+         power_export_l3_(power_export_l3),
+         voltage_l1_(voltage_l1),
+         voltage_l2_(voltage_l2),
+         voltage_l3_(voltage_l3),
+         current_l1_(current_l1),
+         current_l2_(current_l2),
+         current_l3_(current_l3),
+         energy_import_t1_(energy_import_t1),
+         energy_import_t2_(energy_import_t2),
+         energy_export_t1_(energy_export_t1),
+         energy_export_t2_(energy_export_t2),
+         is_three_phase_(true) {}
 
   void setup() override;
   void loop() override;
@@ -46,20 +73,42 @@ class GridMeterComponent : public Component {
   float get_setup_priority() const override { return setup_priority::AFTER_WIFI; }
 
  protected:
-  // Sensors (all required, set via constructor)
-  sensor::Sensor *power_import_;
-  sensor::Sensor *power_export_;
-  sensor::Sensor *voltage_;
-  sensor::Sensor *current_;
+  // Single-phase sensors (backwards compatibility)
+  sensor::Sensor *power_import_{nullptr};
+  sensor::Sensor *power_export_{nullptr};
+  sensor::Sensor *voltage_{nullptr};
+  sensor::Sensor *current_{nullptr};
+
+  // Three-phase sensors
+  sensor::Sensor *power_import_l1_{nullptr};
+  sensor::Sensor *power_import_l2_{nullptr};
+  sensor::Sensor *power_import_l3_{nullptr};
+  sensor::Sensor *power_export_l1_{nullptr};
+  sensor::Sensor *power_export_l2_{nullptr};
+  sensor::Sensor *power_export_l3_{nullptr};
+  sensor::Sensor *voltage_l1_{nullptr};
+  sensor::Sensor *voltage_l2_{nullptr};
+  sensor::Sensor *voltage_l3_{nullptr};
+  sensor::Sensor *current_l1_{nullptr};
+  sensor::Sensor *current_l2_{nullptr};
+  sensor::Sensor *current_l3_{nullptr};
+
+  // Common sensors
   sensor::Sensor *energy_import_t1_;
   sensor::Sensor *energy_import_t2_;
   sensor::Sensor *energy_export_t1_;
   sensor::Sensor *energy_export_t2_;
 
+  bool is_three_phase_;
+
   // Last known good values for voltage and current (hold-on-NaN)
   // Stored as [low_word, high_word] (little-endian word order, matching Reg_s32l)
-  uint16_t voltage_shadow_[2]{0, 0};
-  uint16_t current_shadow_[2]{0, 0};
+  uint16_t voltage_shadow_l1_[2]{0, 0};
+  uint16_t voltage_shadow_l2_[2]{0, 0};
+  uint16_t voltage_shadow_l3_[2]{0, 0};
+  uint16_t current_shadow_l1_[2]{0, 0};
+  uint16_t current_shadow_l2_[2]{0, 0};
+  uint16_t current_shadow_l3_[2]{0, 0};
 
   // Dense register bank covering EM24 addresses 0x0000-0x004F
   uint16_t registers_[REG_COUNT]{};
@@ -78,7 +127,7 @@ class GridMeterComponent : public Component {
   void close_client_(Client &c);
 
   // Sparse register lookup: returns value for any EM24 address, including out-of-dense-range
-  static uint16_t get_register_(const uint16_t *regs, uint16_t addr);
+  uint16_t get_register_(const uint16_t *regs, uint16_t addr);
 
   // Write a signed int32 as two little-endian uint16 registers (Reg_s32l: low word first)
   static void write_int32_(uint16_t *regs, uint8_t idx, int32_t val);
